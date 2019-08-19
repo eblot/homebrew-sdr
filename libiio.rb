6064944f8e851f0ec4d14ca9e79fb69b9551c99b
Emmanuel Blot <emmanuel.blot@free.fr>
2019-08-19 18:02:08 +0200

    Update libiio to v0.18

--- libiio.rb
+++ libiio.rb
@@ -1,8 +1,8 @@
 class Libiio < Formula
   desc "Library for interfacing with IIO devices."
   homepage "https://wiki.analog.com/software/linux/docs/iio/iio"
-  url "https://github.com/analogdevicesinc/libiio/archive/v0.17.tar.gz"
-  sha256 "f591693b8759b6f159ab7afcc1325854a883a486adaf8a849acbdfe0def9db56"
+  url "https://github.com/analogdevicesinc/libiio/archive/v0.18.tar.gz"
+  sha256 "bc2c5299974b65cfe9aa4a06d8c74d7651594e026bce416db48a2c5aa7ba2554"
   head "https://github.com/analogdevicesinc/libiio.git"
 
   depends_on "cmake" => :build
