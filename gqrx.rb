935a63c20dd51a0c2fb52b415d73306492b3f9f0
Emmanuel Blot <emmanuel.blot@free.fr>
2019-08-20 16:48:43 +0200

    Link issue solved for GQRX

--- gqrx.rb
+++ gqrx.rb
@@ -12,12 +12,6 @@ class Gqrx < Formula
   depends_on "eblot/sdr/gnuradio"
   depends_on "eblot/sdr/gr-osmosdr"
 
-  # TODO:
-  # Fix libraries to link, as for now, the final link fails
-  # Add /usr/local/opt/gnuradio/lib/libgnuradio-{runtime,analog,audio,blocks,digital,filter,fft,pmt}.dylib
-  #     /usr/local/opt/log4cpp/lib/liblog4cpp.dylib to build gqrx app
-  # Waiting for GR feedback on how to retrieve the proper libs from CMake macro
-
   def install
     python = Formulary.factory 'python'
     args = %W[
